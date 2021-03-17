f526775f21084eb6d92f5165e9455a9966e92556
aniruddhac13 <aniruddha.chaudhari@iiitb.org>
2021-03-17 16:12:36 +0530

    Added routes for play, pause, next, previous, stop operations for playlist. Added test/demo code via additional buttons in browse.html to demo integration of player.

--- mplayer/mplayer/music/routes.py
+++ mplayer/mplayer/music/routes.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, url_for, flash, redirect, request, Blueprint, jsonify
 from mplayer import db, bcrypt
 from ytmusicapi import YTMusic
+from mplayer.music.vlcpl import *
 
 music = Blueprint('music', __name__)
 ytmusic = YTMusic()
-
+player = playerwrapper()
 
 @music.route('/browse', methods=['POST', 'GET'])
 def browse():
@@ -25,4 +26,56 @@ def background_process():
     except Exception as e:
     	return str(e)
 
+@music.route('/playlist_play')
+def playlist_play():
+    try:
+        playlist_name = request.args.get('playlist_name', '0', type=str)
+        playlist_url = 'playlist://' + playlist_name
+        player.setPlaylist(playlist_url)
+        player.playPlaylist()
+        return jsonify(result='Playing ' + playlist_name)
+
+    except Exception as e:
+    	return str(e)
+
+
+@music.route('/playlist_pause')
+def playlist_pause():
+    try:
+        playlist_name = request.args.get('playlist_name', '0', type=str)
+        player.pausePlaylist()
+        return jsonify(result='Pausing ' + playlist_name)
+
+    except Exception as e:
+    	return str(e)
+
+@music.route('/playlist_next')
+def playlist_next():
+    try:
+        playlist_name = request.args.get('playlist_name', '0', type=str)
+        player.NextInPlaylist()
+        return jsonify(result='Playing next song in ' + playlist_name)
+
+    except Exception as e:
+    	return str(e)
+
+@music.route('/playlist_previous')
+def playlist_previous():
+    try:
+        playlist_name = request.args.get('playlist_name', '0', type=str)
+        player.previousInPlaylist()
+        return jsonify(result='Playing previous song in ' + playlist_name)
+
+    except Exception as e:
+    	return str(e)
+
+@music.route('/playlist_stop')
+def playlist_stop():
+    try:
+        playlist_name = request.args.get('playlist_name', '0', type=str)
+        player.stopPlaylist()
+        return jsonify(result='Stopping ' + playlist_name)
+
+    except Exception as e:
+    	return str(e)
 
